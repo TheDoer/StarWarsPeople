7604472c1bdc68c9004bdbd9d7b6119a4dbeafe0
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2021-10-17 21:58:53 +0200

    details vc

---
+++ StarWarsPeople/ViewController/PeopleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PeopleViewController.swift
+//  StarWarsPeople
+//
+//  Created by Adonis Rumbwere on 17/10/2021.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
